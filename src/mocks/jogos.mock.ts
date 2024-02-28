0eb5a69bd583103be12d0e7e949479d705fede5e
Michely Prado <michely_prado@hotmail.com>
2024-02-28 18:44:54 -0300

    atualização imagens jogos

--- src/mocks/jogos.mock.ts
+++ src/mocks/jogos.mock.ts
@@ -1,4 +1,13 @@
 import { JogoType } from "../types/jogo.type";
+import strategy from "../mocks/images/strategy.jpeg";
+import action from "../mocks/images/action.jpeg";
+import shooter from "../mocks/images/shooter.jpeg";
+import adventure from "../mocks/images/adventure.jpeg";
+import sport from "../mocks/images/sport.jpeg";
+import rpg from "../mocks/images/rpg.jpeg";
+import puzzle from "../mocks/images/puzzle.jpeg";
+import radical from "../mocks/images/radical.jpeg";
+import arcade from "../mocks/images/arcade.jpeg";
 
 const jogosMock: JogoType[] = [
   {
@@ -7,7 +16,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/guerra-das-estrategias',
       urlDemostracao: 'https://example.com/demo/guerra-das-estrategias',
       descricao: 'Construa seu império e domine o mundo neste jogo de estratégia épico.',
-      imagem: 'https://picsum.photos/200/300?random1'
+      imagem: strategy
   },
   {
       nome: 'Ação Espacial: O Retorno',
@@ -15,7 +24,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/acao-espacial',
       urlDemostracao: 'https://example.com/demo/acao-espacial',
       descricao: 'A batalha pelo universo está de volta! Prepare-se para ação intensa e gráficos impressionantes.',
-      imagem: 'https://picsum.photos/200/300?random2'
+      imagem: action
   },
   {
       nome: 'Atirador Galáctico',
@@ -23,7 +32,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/atirador-galactico',
       urlDemostracao: 'https://example.com/demo/atirador-galactico',
       descricao: 'Proteja a galáxia dos invasores alienígenas neste emocionante jogo de tiro em primeira pessoa.',
-      imagem: 'https://picsum.photos/200/300?random3'
+      imagem: shooter
   },
   {
       nome: 'Aventuras Perdidas',
@@ -31,7 +40,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/aventuras-perdidas',
       urlDemostracao: 'https://example.com/demo/aventuras-perdidas',
       descricao: 'Explore terras desconhecidas, resolva enigmas e descubra segredos antigos nesta aventura emocionante.',
-      imagem: 'https://picsum.photos/200/300?random4'
+      imagem: adventure
   },
   {
       nome: 'Futebol Total',
@@ -39,7 +48,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/futebol-total',
       urlDemostracao: 'https://example.com/demo/futebol-total',
       descricao: 'Entre em campo e mostre suas habilidades neste simulador de futebol realista e envolvente.',
-      imagem: 'https://picsum.photos/200/300?random5'
+      imagem: sport
   },
   {
       nome: 'Reinos da Fantasia',
@@ -47,7 +56,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/reinos-da-fantasia',
       urlDemostracao: 'https://example.com/demo/reinos-da-fantasia',
       descricao: 'Escolha seu personagem e embarque em uma jornada épica através de terras mágicas e perigosas.',
-      imagem: 'https://picsum.photos/200/300?random6'
+      imagem: rpg
   },
   {
       nome: 'Quebra-Cabeças Misteriosos',
@@ -55,7 +64,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/quebra-cabecas-misteriosos',
       urlDemostracao: 'https://example.com/demo/quebra-cabecas-misteriosos',
       descricao: 'Resolva enigmas desafiadores e desvende segredos ocultos neste intrigante jogo de quebra-cabeça.',
-      imagem: 'https://picsum.photos/200/300?random7'
+      imagem: puzzle
   },
   {
       nome: 'Operação: Aniquilação',
@@ -63,7 +72,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/operacao-aniquilacao',
       urlDemostracao: 'https://example.com/demo/operacao-aniquilacao',
       descricao: 'Participe de missões secretas e elimine alvos de alto valor neste intenso jogo de tiro tático.',
-      imagem: 'https://picsum.photos/200/300?random8'
+      imagem: shooter
   },
   {
       nome: 'Corrida Radical',
@@ -71,7 +80,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/corrida-radical',
       urlDemostracao: 'https://example.com/demo/corrida-radical',
       descricao: 'Acelere seu carro em pistas desafiadoras e torne-se o campeão definitivo nesta corrida emocionante.',
-      imagem: 'https://picsum.photos/200/300?random9'
+      imagem: radical
   },
   {
       nome: 'Aventuras na Arcádia',
@@ -79,7 +88,7 @@ const jogosMock: JogoType[] = [
       url: 'https://example.com/aventuras-na-arcadia',
       urlDemostracao: 'https://example.com/demo/aventuras-na-arcadia',
       descricao: 'Reviva a nostalgia dos fliperamas com este conjunto de minijogos clássicos e divertidos.',
-      imagem: 'https://picsum.photos/200/300?random10'
+      imagem: arcade
   }
 ];
 
