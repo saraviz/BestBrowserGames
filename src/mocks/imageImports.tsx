b7f61584a46a590d255298e8ba82d5ec22e18117
Gabriela Padilha <139597739+GabiPadilhaf@users.noreply.github.com>
2024-02-29 22:08:52 -0300

    Add files via upload

---
+++ src/mocks/imageImports.tsx
@@ -0,0 +1,21 @@
+import strategy from "../mocks/images/strategy.jpeg";
+import action from "../mocks/images/action.jpeg";
+import shooter from "../mocks/images/shooter.jpeg";
+import adventure from "../mocks/images/adventure.jpeg";
+import sport from "../mocks/images/sport.jpeg";
+import rpg from "../mocks/images/rpg.jpeg";
+import puzzle from "../mocks/images/puzzle.jpeg";
+import radical from "../mocks/images/radical.jpeg";
+import arcade from "../mocks/images/arcade.jpeg";
+
+export default {
+  strategy,
+  action,
+  shooter,
+  adventure,
+  sport,
+  rpg,
+  puzzle,
+  radical,
+  arcade,
+};
